edc985613def496a9b110ed004e03e87d9bb0877
Russell Shurts <russ.shurts@ties.k12.mn.us>
2015-07-20 15:01:18 -0500

    Add --port argument

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+"use strict";
+
 var http = require('http');
 var chalk = require('chalk');
 
-const PORT = 7331;
+function setPort () {
+    var args = process.argv.slice(2);
+    if (args[0] === "--port" && args[1]) {
+        return args[1];
+    } else {
+        return 7331;
+    }
+}
 
-function handleRequest(request, response) {
+function handleRequest (request, response) {
     try {
         var body = "";
         request.on('data', function (chunk) {
@@ -13,6 +22,7 @@ function handleRequest(request, response) {
             console.log(chalk.magenta('Time   : ') + new Date());
             console.log(chalk.magenta('Path   : ') + request.url);
             console.log(chalk.magenta('Method : ') + request.method);
+            console.log(chalk.magenta('Header : ') + JSON.stringify(request.headers));
             console.log(chalk.magenta('Data   : ') + body);
             console.log();
             response.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -25,6 +35,8 @@ function handleRequest(request, response) {
 
 var server = http.createServer(handleRequest);
 
-server.listen(PORT, function() {
-    console.log("Server listing on http://localhost:%s/", PORT);
+var port = setPort();
+
+server.listen(port, function () {
+    console.log("Server listing on http://localhost:%s/", port);
 });
